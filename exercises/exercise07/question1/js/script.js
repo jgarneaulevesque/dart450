c6e9f3ac9903a7224759b66670168e87202552f2
jgarneaulevesque <35274465+jgarneaulevesque@users.noreply.github.com>
2018-03-20 18:58:25 -0400

    Add files via upload

---
+++ exercises/exercise07/question1/js/script.js
@@ -0,0 +1,18 @@
+/**********************************************
+
+DART 450, Winter 2018
+Title of Project
+Author
+
+Description of what the script does...
+
+**********************************************/
+
+$(document).ready(function () {
+
+  // Insert jQuery code here to run when the page is loaded
+//the character voice load onto the page blablabla
+responsiveVoice.speak("Oh sorry! I didn't realize that you didn't know my name, silly me! It is in the title after all. That said, my name is Kawaï Setsuna! Nice to meet you! Say, how do I look? Am I pretty to you?", "US English Female", {pitch: 1.3, volume:1});
+
+
+});
