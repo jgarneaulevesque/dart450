a46045c1a206b24db27473965e55ab127fa8cdef
jgarneaulevesque <35274465+jgarneaulevesque@users.noreply.github.com>
2018-03-20 19:10:01 -0400

    Add files via upload

---
+++ exercises/exercise07/question1/question2/question3_1/js/script.js
@@ -0,0 +1,36 @@
+/**********************************************
+
+DART 450, Winter 2018
+Title of Project
+Author
+
+Description of what the script does...
+
+**********************************************/
+
+$(document).ready(function () {
+
+  // Insert jQuery code here to run when the page is loaded
+  responsiveVoice.speak("I really like role-playing games. It's just so much fun to play a character that we can customize with. We can explore in a fantastic world we rather never see, and beat enormous monsters whenever we want. If only we could experience it in real life. Wouldn't that be fun?", "US English Female", {pitch: 1.3, volume:1});
+
+  //able to click anywhere on the page
+  $(document).click(showSecretMessage);
+  $(document).click(showSecretMessage2);
+//show hidden button and message2
+  $('#message2').click(showmenu);
+
+
+
+});
+
+function showSecretMessage() {
+  $('#message').show();
+}
+
+function showSecretMessage2() {
+  $('#message2').show();
+}
+
+function showmenu() {
+  $('#menu').show();
+}
