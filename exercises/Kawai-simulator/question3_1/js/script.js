18d081474b443650e57589d9a6f8b7d25703634c
jgarneaulevesque <35274465+jgarneaulevesque@users.noreply.github.com>
2018-04-19 09:32:06 -0400

    Add files via upload

---
+++ exercises/Kawai-simulator/question3_1/js/script.js
@@ -0,0 +1,47 @@
+/**********************************************
+
+DART 450, Winter 2018
+Title of Project
+Author
+
+Description of what the script does...
+
+**********************************************/
+
+$(document).ready(function () {
+
+    // Insert jQuery code here to run when the page is loaded
+
+//music audio volume
+  var audio = document.getElementById("music");
+    audio.volume = 0.2;
+
+//animation expression character with fadeIn
+  setTimeout(function(){
+  $(".character2").fadeIn();
+
+},16000);
+
+//the character voice load onto the page blablabla
+  responsiveVoice.speak("I really like role-playing games. It's just so much fun to play a character that we can customize with. We can explore in a fantastic world we rather never see, and beat enormous monsters whenever we want. If only we could experience it in real life. Wouldn't that be fun?", "US English Female", {pitch: 1.3, volume:1});
+
+  //able to click anywhere on the page
+  $(document).click(showSecretMessage);
+  $(document).click(showSecretMessage2);
+//show hidden button and message2
+  $('#message2').click(showmenu);
+
+});
+
+//click to appear a secret message of the character inner thought
+function showSecretMessage() {
+  $('#message').show();
+}
+
+function showSecretMessage2() {
+  $('#message2').show();
+}
+
+function showmenu() {
+  $('#menu').show();
+}
